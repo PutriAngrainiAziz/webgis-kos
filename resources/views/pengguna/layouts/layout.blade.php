d7ed3d885ab87a5a84c5aa03e4478f86ee8f2d5f
PutriAngrainiAziz <putriangrainiaziz27@gmail.com>
2025-07-05 21:33:20 +0700

    mengembalikan footer copyright

--- resources/views/pengguna/layouts/layout.blade.php
+++ resources/views/pengguna/layouts/layout.blade.php
@@ -144,9 +144,15 @@
                 </div>
             </div>
 
-            <div class="text-center mt-4">
-                <p class="mb-0">&copy; <strong>SIG Kos-kosan</strong> - Kambu, Kendari</p>
-                <small>Designed by <a href="#" class="text-light">Kelompok haa</a></small>
+            <div class="container copyright text-center mt-4">
+            <p>© <span>Copyright</span> <strong class="px-1 sitename">Imperial</strong> <span>All Rights Reserved</span></p>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you've purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
             </div>
         </div>
     </footer>
